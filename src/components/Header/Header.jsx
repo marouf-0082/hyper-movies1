2014ce5108d8565dd4d1f5a5963868b6d8623828
next1code <next1code@yahoo.com>
2026-01-05 21:53:45 +0430

    Use , useLocation with some condition

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,22 +4,26 @@ import HeaderSlider from "./HeaderSlider";
 import Navigation from "./Navigation";
 import SearchBox from "./SearchBox";
 import cinema from "../../Assets/images/cinema.jpg";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
   const [bg, setBg] = useState(cinema);
 
   return (
     <header
-      className="transition-all duration-500 bg-cover pb-8 bg-center md:py-8"
+      className={`transition-all duration-500 bg-cover pb-8 bg-center md:py-8 ${location.pathname != "/" ? "h-[600px]" : ""}`}
       style={{
         backgroundImage: `linear-gradient(to bottom, rgb(30 41 59 / 100%), rgb(30 41 59 / 60%), rgb(30 41 59 / 30%)), url('${bg}')`,
       }}
     >
-        <Navigation />
+      <Navigation />
       <div className="container">
         <SearchBox />
-        <FollowUs />
-        <HeaderSlider setBg={setBg} />
+          <div className={`${location.pathname !== '/' ? 'hidden' : 'block'}`}>
+            <FollowUs />
+            <HeaderSlider setBg={setBg} />
+          </div>
       </div>
     </header>
   );
