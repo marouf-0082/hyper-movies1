d3b96a6dd435965ad87750616c8b3bdac0f56ea9
next1code <next1code@yahoo.com>
2026-02-19 23:09:53 +0430

    Fix import path for userContext in Login component

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { userContext } from "../context/Usercontext";
+import { userContext } from "../context/Usercontext.jsx";
 
 export default function Login() {
   const { Login, session } = useContext(userContext);
